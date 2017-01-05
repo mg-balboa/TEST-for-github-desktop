eeaad8c75ed008e77085e7d2dcdd06617cad5f4a
Marcello Balboa <marcello.balboa@yahoo.com>
2017-01-05 04:45:04 -0500

    Initial Commit

---
+++ TEST for github desktop/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TEST for github desktop
+//
+//  Created by Marcello Balboa on 1/5/17.
+//  Copyright © 2017 New Image Development. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
